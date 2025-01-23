0c5915a5a22fe446841ceda3180a8b9eab303a4a
Yamizaki <Arakaki_18@outlook.com>
2025-01-23 16:26:12 -0500

    Using ASGI

--- practicaCrud/wsgi.py
+++ practicaCrud/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+# asgi.py
 import os
+from django.core.asgi import get_asgi_application
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practicaCrud.settings')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "practicaCrud.settings")
-
-application = get_wsgi_application()
+application = get_asgi_application()
